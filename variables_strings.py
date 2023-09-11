8a2cfb815526ff59cbf873f4b608b447186f1b73
olaoluwabaggio <olaoluwa.ogunsola@gmail.com>
2023-09-11 01:18:00 +0000

    Strings

---
+++ variables_strings.py
@@ -0,0 +1,9 @@
+first_name = "Olaoluwa"
+last_name = "Ogunsola"
+print(first_name + " " + last_name)
+
+
+print("My first name is {}. My family name is {}".format(first_name, last_name))
+
+
+print(f"My firstname is {first_name}. My last name is {last_name}")
